197d0872ad78981fecbbc874fdaeaa364ec9b20c
Алексей Лепёшкин <33727336+AlexeyLyapeshkin@users.noreply.github.com>
2019-02-20 20:38:42 +0300

    Update

--- my_client.py
+++ my_client.py
@@ -2,61 +2,93 @@ import socket
 import pickle
 import time
 import threading
+import sys
 
-stop = False
+
+def tester(host='192.168.0.102', port=9090):
+    s = socket.socket()
+    s.settimeout(5)
+    answer = s.connect_ex((host, port))
+    if answer == 10061:
+        s.close()
+        return True
+    else:
+        s.close()
+        return False
+
 
 def receving(sock):
+    stop = False
     while not stop:
         try:
             while True:
                 data, addr = sock.recvfrom(1024)
                 unpick = pickle.loads(data)
-                print('['+unpick['Name']+']:' + unpick['Message']+'\n')
+                print('[' + unpick['Name'] + ']: ' + unpick['Message'] + '\n')
                 time.sleep(0.2)
         except:
+            stop = True
             sock.close()
 
 
-main_message = ' {0} is connected! '
-left_message = ' {0} is left chat! '
+def main(ip, port):
+    stop = False
+    main_message = ' {0} is connected! '
+    left_message = ' {0} is left chat! '
 
-host = socket.gethostbyname(socket.gethostname())
-port = 0
 
-server = ("192.168.0.102", 9090)
+    server = (ip, port)
 
-channel = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-channel.bind((host, port))
-channel.setblocking(1)
+    channel = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    channel.bind((ip, 0))
+    channel.setblocking(1)
 
-print('{0:-^51}'.format('[Client started]'))
+    print('{0:-^51}'.format('[Client started]'))
 
-print("Please, enter you name:")
-nickname = input('>>> ')
-print('-'*60)
+    print("Please, enter you name:")
+    nickname = input('>>> ')
+    print('-' * 60)
 
-thread = threading.Thread(target=receving, args=[channel])
-thread.start()
+    thread = threading.Thread(target=receving, args=[channel])
+    thread.start()
 
-data = {'Name': nickname, 'Message': main_message.format(nickname)}
-datapickle = pickle.dumps(data)
-channel.sendto(datapickle, server)
+    data = {'Name': nickname, 'Message': main_message.format(nickname)}
+    datapickle = pickle.dumps(data)
+    channel.sendto(datapickle, server)
 
-while not stop:
-    try:
-        message = input('\n')
-        if message:
-            data = {'Name': nickname, 'Message': message}
+    while not stop:
+        try:
+            message = input('\n')
+            if message:
+                data = {'Name': nickname, 'Message': message}
+                datapickle = pickle.dumps(data)
+
+                channel.sendto(datapickle, server)
+            time.sleep(0.2)
+        except:
+            stop = True
+            print('{0:-^51}'.format('[Client stoped]'))
+            data = {'Name': nickname, 'Message': left_message.format(nickname), 'Code': 1}
             datapickle = pickle.dumps(data)
-
             channel.sendto(datapickle, server)
-        time.sleep(0.2)
-    except:
-        stop = True
-        print('{0:-^51}'.format('[Client stoped]'))
-        data = {'Name': nickname, 'Message': left_message.format(nickname),'Code':1}
-        datapickle = pickle.dumps(data)
-        channel.sendto(datapickle, server)
+    channel.close()
+    sys.exit()
 
-channel.close()
-thread.join()
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        ip, port = sys.argv[1], sys.argv[2]
+        if tester(ip, int(port)):
+            main(ip, int(port))
+        else:
+            print('__LOCAL_MODE__')
+            main('192.168.0.102', 9090)
+    else:
+        print('__LOCAL_MODE__')
+        main('192.168.0.102', 9090)
--- my_server.py
+++ my_server.py
@@ -1,48 +1,70 @@
-import socket,time
+import socket, time
 import pickle
-
-host = socket.gethostbyname(socket.gethostname())
-port = 9090
+import sys
 
 
-clients = {}
-
-channel = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-channel.bind((host,port))
-print('{0:-^51}'.format('[Server started]'))
-log_file = open('LOG.txt','w')
-stop = False
-while not stop:
-    try:
-        # RECIVE DATA
-        data, adr = channel.recvfrom(1024)
-        unpick = pickle.loads(data) # UNPACK DATA
+def tester(host='192.168.0.102', port=9090):
+    s = socket.socket()
+    s.settimeout(5)
+    answer = s.connect_ex((host, port))
+    if answer == 10061:
+        s.close()
+        return True
+    else:
+        s.close()
+        return False
 
 
-        # NEW USERS
-        if adr not in clients.values():
-            clients[unpick['Name']] = adr
+def main(ip, port):
+    stop = False
+    print('{0:^20} - {1:^20}'.format(ip, port))
+    clients = {}
 
+    channel = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    channel.bind((ip, port))
+    print('{0:-^51}'.format('[Server started]'))
+
+    while not stop:
         try:
-            if unpick['Code'] == 1:
-                del clients[unpick['Name']]
-        except KeyError:
-            pass
+            # RECIVE DATA
+            data, adr = channel.recvfrom(1024)
+            unpick = pickle.loads(data)  # UNPACK DATA
 
-        if clients:
-            for client in clients:
-                if clients[client] != adr:
-                    channel.sendto(data,clients[client])
+            # NEW USERS
+            if adr not in clients.values():
+                clients[unpick['Name']] = adr
+
+            try:
+                if unpick['Code'] == 1:
+                    del clients[unpick['Name']]
+            except:
+                pass
+
+            if clients:
+                for client in clients:
+                    if clients[client] != adr:
+                        channel.sendto(data, clients[client])
+            # LOG
+            itsatime = time.strftime("%Y.%m.%d - %H:%M:%S", time.localtime())
+            print(
+                "[" + adr[0] + "] - [" + str(adr[1]) + "] - [" + itsatime + "] - [" + unpick['Name'] + "] >> " + unpick[
+                    'Message'], end="\n")
+        except:
+            print('{0:-^51}'.format('[Server stoped]'))
+            stop = True
+
+
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        ip, port = sys.argv[1], sys.argv[2]
+        if tester(ip, int(port)):
+            main(ip, int(port))
         else:
-            pass
-
-        # LOG
-        itsatime = time.strftime("%Y.%m.%d - %H:%M:%S", time.localtime())
-        print("[" + adr[0] + "] - [" + str(adr[1]) + "] - [" + itsatime + "] - [" + unpick['Name'] + "] >> "+unpick['Message'], end="\n")
-        time.sleep(0.1)
-    except KeyboardInterrupt:
-        print('{0:-^51}'.format('[Server stoped]'))
-        stop = True
-
-
-channel.close()
+            print('__LOCAL_MODE__')
+            main('192.168.0.102', 9090)
+    else:
+        print('__LOCAL_MODE__')
+        main('192.168.0.102', 9090)
